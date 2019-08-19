70bc37ed5b2d963019bc037eabf27c76473c521e
Nicolaie Constantinescu <kosson@gmail.com>
2019-08-19 15:51:14 +0300

    Lading modificat plus

---
+++ public/js/custom.js
@@ -0,0 +1,63 @@
+/*========== NAVBAR TRANSPARENT TO SOLID ==========*/
+
+$(document).ready(function() { //when document(DOM) loads completely. 
+    // Transition effect for navbar 
+    $(window).scroll(function() { //function is called while you scrolls 
+      // checks if window is scrolled more than 300px, adds/removes solid class
+      if($(this).scrollTop() > 300) { 
+          $('.navbar').addClass('solid'); //add class 'solid' in any element which has class 'navbar'
+      } else {
+          $('.navbar').removeClass('solid'); //remove class 'solid' in any element which has class 'navbar'
+      }
+    });
+});
+
+/*========== CLOSE MOBILE NAV ON CLICK ==========*/
+
+$(document).ready(function () { //when document loads completely.
+$(document).click(function (event) { //click anywhere
+    var clickover = $(event.target); //get the target element where you clicked
+    var _opened = $(".navbar-collapse").hasClass("show"); //check if element with 'navbar-collapse' class has a class called show. Returns true and false.
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) { // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
+        $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
+    }
+});
+});
+
+/*========== SMOOTH SCROLLING TO LINKS ==========*/
+
+$(document).ready(function(){ //document is loaded
+// Add smooth scrolling to all links
+$("a").on('click', function(event) {//click on any link;anchor tag;
+
+// Make sure this.hash has a value before overriding default behavior
+if (this.hash !== "") { //for e.g. website.com#home - #home
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash
+  var hash = this.hash;
+  //console.log('hash:',hash)
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({ //animate whole html and body elements
+    scrollTop: $(hash).offset().top //scroll to the element with that hash
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash; //website.com - website.com#home
+    //Optional remove "window.location.hash = hash;" to prevent transparent navbar on load
+  });
+} // End if
+});
+});
+
+/*========== BOUNCING DOWN ARROW ==========*/
+//down arrow at top
+$(document).ready(function(){
+$(window).scroll(function(){ //browser scroll 
+$(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); //set opacity css from 1 to -(negative) infinity of element with class 'arrow'
+//250 is fade pixels
+});
+});
