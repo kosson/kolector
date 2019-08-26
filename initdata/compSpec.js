c8faad0210604225ad314cfb480a4c011dfa4978
Nicolaie Constantinescu <kosson@gmail.com>
2019-08-26 08:22:38 +0300

    Folding data complete!

--- initdata/compSpec.js
+++ initdata/compSpec.js
@@ -1,54 +1,87 @@
 const fs = require('fs');
-const { Transform } = require('stream');
-
 const Papa = require('papaparse');
-
 const readF = fs.createReadStream('CSuri01.csv', 'utf8');
-const writeF = fs.createWriteStream('CSuri01.json', 'utf8');
+// const writeF = fs.createWriteStream('CSuriX.json', 'utf8');
 
 Papa.parse(readF, {
     header: true,
+    encoding: 'utf8',
+    transformHeader: function (header) {
+        // console.log(header);
+        if (header === 'nume') return 'nume';
+        if (header === 'ids') return 'ids';
+        if (header === 'cod') return 'cod';
+        if (header === 'activitate') return 'activitati';
+        if (header === 'disciplină') return 'disciplina';
+        if (header === 'nivel') return 'nivel';
+        if (header === 'act normativ') return 'ref';
+        if (header === 'competență generală') return 'parteA';
+        if (header === 'număr competența generală') return 'compGen';
+    },
     transform: function (value, headName) {
         // console.log(headName);
         // Array.isArray(obj) ? obj:[obj]
         if (headName === 'nume') {
             return value;
-        }
-        
-        if (headName === 'ids') {
+        } else if (headName === 'ids') {
             value = [].concat(value);
             return value;
         } else if ((headName === 'cod')) {
             return value;
-        } else if (headName === 'activitate') {
-            headName = 'activitati';
+        } else if (headName === 'activitati') {
             value = [].concat(value);
-            return headName, value;
-        } else if (headName === 'disciplină') {
-            headName = 'disciplina';
+            return value;
+        } else if (headName === 'disciplina') {
             value = [].concat(value);
             return value;           
         } else if (headName === 'nivel') {
             value = [].concat(value);
             return value;
-        } else if (headName === 'act normativ') {
-            headName = 'ref';
+        } else if (headName === 'ref') {
             value = [].concat(value);
             return value;
-        } else if (headName === 'competență generală') {
-            headName = 'parteA';
+        } else if (headName === 'parteA') {
+            return value;
+        } else if (headName === 'compGen') {
             return value;
         }
     },
-    complete: function (result) {
-        // if (result.errors) throw result.errors;
-        // result.data.reduce((ac, curr, idx, arr) => {
-        //     if (ac.ids === curr.ids) {
-        //         ac.activitate
-        //     }
-        // });
-        fs.writeFile('CSuri01.json', JSON.stringify(result.data), 'utf8', (err) => {
+    complete: function (results, file) {
+        if (results.errors) {
+            console.log(results.errors);
+        }
+        const folded = foldOneField(results.data);
+        fs.writeFile('CSuriX.json', JSON.stringify(folded), 'utf8', (err) => {
             if (err) throw err;
         });
+    },
+    error: function (err, file) {
+        if (err) {
+            console.log(err.message);
+        }
     }
 });
+
+/**
+ * Funcția are rolul de a strânge toate activitățile unei competențe specifice într-un array dedicat.
+ * Când sunt erori, problema stă în normalizarea datelor. ATENȚIE! M-am opărit!
+ * @param {Object} data Este un obiect de date
+ */
+function foldOneField (data) {
+    const arr = JSON.stringify(data);
+    const folded = data.reduce((arrAcc, elemArrOrig, idx, srcArr) => {
+        if (arrAcc.length === 0) {
+            arrAcc[idx] = elemArrOrig;
+            // console.log(arrAcc.slice(-1)[0].ids[0]);
+        } 
+        if (arrAcc.slice(-1)[0].ids[0] === elemArrOrig.ids[0]) {
+            elemArrOrig.activitati.forEach((act) => {
+                arrAcc.slice(-1)[0].activitati.push(act);
+            });
+        } else {
+            arrAcc.push(srcArr[idx]);
+        }
+        return arrAcc;
+    }, []);
+    return folded;
+}
---
+++ initdata/limboCode.js.js
@@ -0,0 +1,112 @@
+var x = [{
+    "nume": "Identificarea unor caracteristici ale corpurilor vii și nevii",
+    "ids": ["1.1."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.1",
+    "activitati": ["observarea unor corpuri și identificarea caracteristicilor acestora (de exemplu: organisme vii pentru identificarea părților componente, diferite obiecte pentru identificarea unor proprietăți precum formă, culoare, transparență)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Identificarea unor caracteristici ale corpurilor vii și nevii",
+    "ids": ["1.1."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.1",
+    "activitati": ["utilizarea unor modele pentru identificarea caracteristicilor principale ale corpurilor reprezentate (de exemplu: schițe de hărți pentru observarea suprafețelor de apă și uscat, mulaje ale unor tipuri de animale din diferite grupe)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Identificarea unor caracteristici ale corpurilor vii și nevii",
+    "ids": ["1.1."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.1",
+    "activitati": ["observarea dirijată a unor scheme simple, desene pentru identificarea\netapelor unor fenomene/ procese (de exemplu: circuitul apei în natură)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Identificarea unor caracteristici ale corpurilor vii și nevii",
+    "ids": ["1.1."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.1",
+    "activitati": ["recunoașterea unor caracteristici ale corpurilor pornind de la prezentarea unor texte științifice adaptate vârstei elevilor, a unor povești sau povestiri (de exemplu: descrierea unor reacții ale animalelor în diferite situații)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Identificarea unor caracteristici ale corpurilor vii și nevii",
+    "ids": ["1.1."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.1",
+    "activitati": ["observarea unor aspecte dinamice ale realității înconjurătoare sau mai îndepărtate de mediul de viață cunoscut, prin vizionarea unor filme sau realizarea unor jocuri de rol (de exemplu, despre mișcarea apei pe suprafața Pământului, despre modul în care căderea  vântul pot\nproduce energie)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Identificarea unor caracteristici ale corpurilor vii și nevii",
+    "ids": ["1.1."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.1",
+    "activitati": ["identificarea unor caracteristici ale corpurilor, fenomenelor, proceselor prin efectuarea unor experiențe simple (de exemplu: evidențierea diferențelor dintre diferite surse de apă, evidențierea mișcărilor apei pe suprafața planetei, evidențierea schimbării stării de agregare a apei și a relației cu temperatura, evidențierea mișcării aerului și a influenței vântului asupra norilor, evidențierea unor interacțiuni între corpuri și a efectelor lor)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Utilizarea unor criterii pentru compararea unor corpuri, fenomene și procese",
+    "ids": ["1.2."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.2",
+    "activitati": ["identificarea unor criterii de comparare a unor corpuri, fenomene, procese din aceeași categorie (de exemplu: diverse proprietăți ale metalelor, diverse caracteristici ale unor animale)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Utilizarea unor criterii pentru compararea unor corpuri, fenomene și procese",
+    "ids": ["1.2."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.2",
+    "activitati": ["compararea unor corpuri, fenomene, procese din aceeași categorie în scopul stabilirii asemănărilor și deosebirilor între ele (de exemplu: compararea metalelor după diferite proprietăți, a animalelor după diferite caracteristici)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}, {
+    "nume": "Utilizarea unor criterii pentru compararea unor corpuri, fenomene și procese",
+    "ids": ["1.2."],
+    "cod": "stiinte-identificare-caracteristici-cl3-1.2",
+    "activitati": ["selectarea unor corpuri după diferite criterii și realizarea unor colecții/ expoziții (de exemplu: colecții de obiecte selectate după formă, mărime, culoare, întrebuințări, colecții de plante sau părți componente ale acestora)"],
+    "disciplina": ["Științe ale naturii,  clasa a III-a- a IV-a"],
+    "nivel": ["Clasa a III-a"],
+    "ref": ["OM 5003 / 02.12.2014"],
+    "parteA": "Explorarea caracteristicilor unor corpuri, fenomene și procese",
+    "compGen": "1"
+}];
+
+const y = x.reduce((arrAcc, elemArrOrig, idx, srcArr) => {
+    if (arrAcc.length === 0) {
+        arrAcc[idx] = elemArrOrig;
+        // console.log(arrAcc.slice(-1)[0].ids[0]);
+    } 
+    if (arrAcc.slice(-1)[0].ids[0] === elemArrOrig.ids[0]) {
+        elemArrOrig.activitati.forEach((act) => {
+            arrAcc.slice(-1)[0].activitati.push(act);
+        });
+        // compara următorul obiect din array-ul originar cu cel din acumulator
+        // dacă ids-urile corespund, adaugă activitatea la cel preexistent
+        // dacă nu corespund, adaugă obiectul din array-ul original la acumulator.
+    } else {
+        arrAcc.push(srcArr[idx])
+    }
+
+    return arrAcc;
+}, []);
+console.log(y);
+console.log(y.length);
