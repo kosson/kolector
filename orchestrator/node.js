6af4b8b1a3a7d13b377f6f5ff48d2859443cdf4d
kosson <kosson@gmail.com>
2023-07-06 14:56:07 +0300

    return to zero... mf!

--- orchestrator/node.js
+++ orchestrator/node.js
@@ -8,3 +8,64 @@ import { webSockets } from '@libp2p/websockets';
 import { identifyService } from 'libp2p/identify';
 import {MemoryBlockstore} from 'blockstore-core';
 import {MemoryDatastore} from 'datastore-core';
+import {unixfs} from '@helia/unixfs';
+import { json } from '@helia/json';
+import { strings } from '@helia/strings';
+import { logger } from '@libp2p/logger';
+import last from 'it-last';
+async function createNode () {
+    try {
+        // creează un blockstore nou
+        const blockstore = new MemoryBlockstore();
+        // creează un datastore nou
+        const datastore = new MemoryDatastore(); // necesar pentru a stoca info specifice (căutări DHT, etc)
+    
+        const libp2p = await createLibp2p({
+            addresses: {
+                listen: ['/ip4/0.0.0.0/tcp/0'] // a public P2P swarm port
+            },
+            transports: [
+                webSockets(),
+                tcp()
+            ],
+            connectionEncryption:[noise()],
+            streamMuxers: [yamux()],
+            datastore,
+            identity:{
+                host: {
+                    agentVersion: 'helia/0.0.0'
+                }
+            },
+            nat: {enabled: false},
+            relay: {enabled: false},
+            peerDiscovery: [
+                bootstrap({
+                  list: [
+                    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+                    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+                    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+                    "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
+                  ]
+                })
+            ],
+            services: {
+                identify: identifyService()
+            }
+        });
+    
+        const log = logger('helia');
+
+        // creează un nod Helia
+        return await createHelia({
+            libp2p,
+            blockstore,
+            datastore
+        });
+    } catch (error) {
+        if (error) {
+            throw new Error(`la crearea nodului s-a întâmplat ceva neașteptat: ${error}`);
+        };   
+    }
+};
+
+export const knode = await createNode();
